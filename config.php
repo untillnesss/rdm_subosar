3a8a5233d40133709eb1b06432040c5ba75324a2
yasa <pvzuntil@gmail.com>
2021-07-13 14:02:59 +0700

    from server

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$host = "localhost";
+$databaseuser = "sangpeme_untillness";
+$databasename = "sangpeme_rdm_subosar";
+$dbpassword = "untillNess1013";
+define("DB_SERVER",$host);
+define("DB_USERNAME",$databaseuser);
+define("DB_PASSWORD",$dbpassword);
+define("DB_DATABASE",$databasename);
+?>
